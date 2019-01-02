5631fc0fdb06c63d0416d3e898ecd17f96318b3a
daenorks <hspinat.geek@gmail.com>
2019-01-03 00:49:30 +0100

    Domino update

--- ProjectD/src/domino/DPlateau.java
+++ ProjectD/src/domino/DPlateau.java
@@ -8,7 +8,7 @@ public class DPlateau extends Plateau {
 		return (check(d.getCarre1(), x1, y1) && check(d.getCarre2(), x2, y2));
 	}
 	
-	boolean put(Domino d, int x1, int y1, int x2, int y2) {
+	void put(Domino d, int x1, int y1, int x2, int y2) {
 		put(d.getCarre1(), x1, y1);
 		put(d.getCarre2(), x2, y2);
 	}
---
+++ ProjectD/src/saboteur/Main.java
@@ -0,0 +1,5 @@
+package saboteur;
+
+public class Main {
+
+}
