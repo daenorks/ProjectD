13fa96a64cd4d4fd845dc320f493a535c2186fbe
Julien BERSAUTER <julien.bersauter@orange.fr>
2019-01-03 02:03:36 +0100

    push
    push

--- ProjectD/src/domino/Domino_Jeu.java
+++ ProjectD/src/domino/Domino_Jeu.java
@@ -1,10 +1,13 @@
 public class Domino_Jeu {
 	
 	private DPlateau plateau;
-	private ArrayList<Joueur> joueurs;
+	private LinkedList<Joueur> joueurs;
+	private LinkedList<Joueur> classement;
+	private DPioche pioche;
 	
 	public Domino_Jeu (){
 		joueurs = new ArrayList<Joueur>;
+		classement = new ArrayList <Joueur>;
 	}
 	
 	public void add (Joueur j) {
@@ -12,6 +15,10 @@ public class Domino_Jeu {
 	}
 	
 	public void demarrer(int x){
+		if (joueurs.size() == 0) {
+			// leve une exception
+			return;
+		}
 		distribuer(x);
 		plateau = new DPlateau (x);
 		jouer();
@@ -19,13 +26,21 @@ public class Domino_Jeu {
 	
 	public void distribuer(int x) { // distribue X dominos au hasard à chaque joueurs
 		for (int i = 0 ; i < joueurs.size() ; i++) {
-			joueurs(i).add(hasard(x))
+			joueurs(i).add(hasard(x));
 		}
 	}
 	
 	
 	public void jouer() {
-		
+		for(int i = 0 ; i < joueurs.size(); i++) {
+			if (joueurs.get(i).nombreDeCartes() == 1) {
+				if (joueurs.poser()) {
+					classement.add(joueurs.get(i));
+					joueurs.remove(i);
+				}
+			}
+		}
+		jouer();
 	}
 	
 	
