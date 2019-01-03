83725356365d7743008d840507838f08a77aab5e
Julien BERSAUTER <julien.bersauter@orange.fr>
2019-01-03 01:18:34 +0100

    Create Domino_Jeu.java

---
+++ ProjectD/src/domino/Domino_Jeu.java
@@ -0,0 +1,17 @@
+public class Domino_Jeu {
+	
+	private DPlateau plateau;
+	private ArrayList<Joueur> joueurs;
+	
+	public Domino_Jeu (){
+		joueurs = new ArrayList<Joueur>;
+	}
+	
+	public void add (Joueur j) {
+		joueurs.add(j);
+	}
+	
+	public void demarrer(){
+		
+	}
+}
